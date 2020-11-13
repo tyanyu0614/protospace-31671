7078ddb2ec1d646b587aef88458fc6576a935834
tyanyu0614 <73101963+tyanyu0614@users.noreply.github.com>
2020-11-13 17:09:18 +0900

    modifying some codes

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -51,7 +51,6 @@ end
     params.require(:prototype).permit(:title, :image, :catch_copy, :concept).merge(user_id: current_user.id)
   end
 
-  unless current_user_signed_in?
-  redirect_to action: :index
-end
+  
+
 end
